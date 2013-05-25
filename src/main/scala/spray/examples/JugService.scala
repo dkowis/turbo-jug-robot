fac063cc8122045a5efc38efffd76d1e827c5f83
David Kowis <dkowis@shlrm.org>
2013-05-25 18:06:06 -0500

    I can't get spray.io to cooperate yet.
    Even serving up simple stupid static files doesn't work. I'm done here.

--- src/main/scala/spray/examples/JugService.scala
+++ src/main/scala/spray/examples/JugService.scala
@@ -5,37 +5,22 @@ import spray.http.MediaTypes
 import MediaTypes._
 
 trait JugService extends HttpService {
-  val jugRoute = path("") {
-    get {
-      respondWithMediaType(`text/html`) {
-        complete {
-          <html>
-            <body>
-              <h1>Say hello to
-                <i>spray-servlet</i>
-                !</h1>
-              <p>Defined resources:</p>
-              <ul>
-                <li>
-                  <a href="/ping">/ping</a>
-                </li>
-                <li>
-                  <a href="/stream">/stream</a>
-                </li>
-                <li>
-                  <a href="/crash">/crash</a>
-                </li>
-                <li>
-                  <a href="/timeout">/timeout</a>
-                </li>
-                <li>
-                  <a href="/timeout/timeout">/timeout/timeout</a>
-                </li>
-              </ul>
-            </body>
-          </html>
+  val jugRoute = path("js") {
+    getFromBrowseableDirectory("javascripts") //this *should* get the javascripts from teh war file...
+  } ~
+    //Second route to get the index page
+    path("") {
+      get {
+        respondWithMediaType(`text/html`) {
+          complete {
+            <html>
+              <body>
+                <h1>Hello world</h1>
+              </body>
+            </html>
+          }
         }
       }
     }
-  }
+
 }
