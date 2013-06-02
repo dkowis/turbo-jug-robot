6ab87acc5020c14103c96b7193aa6e0f14811670
David Kowis <dkowis@shlrm.org>
2013-06-02 16:02:38 -0500

    Hrm, doesn't work...

--- src/main/scala/org/shlrm/jugbot/Main.scala
+++ src/main/scala/org/shlrm/jugbot/Main.scala
@@ -5,7 +5,7 @@ import akka.actor.Props
 
 object Main extends App with SprayCanHttpServerApp with MySslConfiguration {
 
-  val handler = system.actorOf(Props[DemoService])
+  val handler = system.actorOf(Props[JugServiceActor])
 
   //create a new http server using our handler and tell it what to bind to
   newHttpServer(handler) ! Bind(interface = "localhost", port = 8080)
