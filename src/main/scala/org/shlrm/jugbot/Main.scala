845c250e6ba79ccd9e1b7ec3df374ec5d6a9c462
David Kowis <dkowis@shlrm.org>
2013-06-02 16:32:19 -0500

    Trying to clean out things I'm not using

--- src/main/scala/org/shlrm/jugbot/Main.scala
+++ src/main/scala/org/shlrm/jugbot/Main.scala
@@ -3,11 +3,11 @@ package org.shlrm.jugbot
 import spray.can.server.SprayCanHttpServerApp
 import akka.actor.Props
 
-object Main extends App with SprayCanHttpServerApp with MySslConfiguration {
+//TODO: would have to add back in SSL support if I wants to use it
+object Main extends App with SprayCanHttpServerApp {
 
-  val handler = system.actorOf(Props[JugServiceActor])
+  val handler = system.actorOf(Props[JugServiceActor], "jug-service")
 
   //create a new http server using our handler and tell it what to bind to
   newHttpServer(handler) ! Bind(interface = "localhost", port = 8080)
-
 }
