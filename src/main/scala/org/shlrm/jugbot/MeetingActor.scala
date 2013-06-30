0eecdc9e7b50c6ece8dfbc9f60f70b843bf7ffd2
David Kowis <dkowis@shlrm.org>
2013-06-29 20:52:19 -0500

    Log messages for the demo

--- src/main/scala/org/shlrm/jugbot/MeetingActor.scala
+++ src/main/scala/org/shlrm/jugbot/MeetingActor.scala
@@ -15,19 +15,23 @@ class MeetingActor extends Actor with SprayActorLogging with AskSupport {
   def receive = {
     //CASE CLASS PARENS MURDER ME
     case ListMeetings() => {
-     log.info("Goign to try to list meetings")
+     log.info("Listing meetings")
       sender ! ms.listMeetings
     }
     case GetMeeting(meetingId) => {
+      log.info(s"Getting a meeting by id: ${meetingId}")
       sender ! ms.getMeeting(meetingId)
     }
     case CreateMeeting(meeting) => {
+      log.info(s"creating a meeting: ${meeting}")
       sender ! ms.createMeeting(meeting)
     }
     case UpdateResults(meetingId, answers) => {
+      log.info(s"Updating results for a meeting, ${meetingId}: ${answers}")
       ms.updateResults(meetingId, answers)
     }
     case SurveyResults(meetingId) => {
+      log.info(s"getting survey results for ${meetingId}")
       sender ! ms.surveyResults(meetingId)
     }
     case _ => {
