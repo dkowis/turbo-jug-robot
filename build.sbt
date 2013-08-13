b01c5ced104d11e3f697862ce78ff543a89c7096
David Kowis <dkowis@shlrm.org>
2013-08-12 21:42:53 -0500

    Defined my dependencies in one location

--- build.sbt
+++ build.sbt
@@ -9,27 +9,7 @@ version := "1.0"
 scalaVersion := "2.10.0"
 
 //Using spray.io versions 1.1-M8
-libraryDependencies ++= Seq (
-    "net.databinder.dispatch" %% "dispatch-core" % "0.10.1" % "test",
-    "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test",
-    "io.spray" % "spray-testkit" % "1.1-M8" % "test",
-    "com.googlecode.flyway" % "flyway-core" % "2.1.1",
-    "com.typesafe" % "config" % "1.0.2",
-    //Adding JSON support using spray-json
-    "io.spray" %%  "spray-json" % "1.2.5",
-    "io.spray" % "spray-can" % "1.1-M8",
-    "io.spray" % "spray-routing" % "1.1-M8",
-    "com.typesafe.akka" %% "akka-actor" % "2.1.4",
-    "com.typesafe.akka" %% "akka-slf4j" % "2.1.4",
-    "ch.qos.logback" % "logback-classic" % "1.0.12",
-    //Lets add the cucumber-jvm stuff and make cucumbers happen
-    "info.cukes" % "cucumber-junit" % "1.1.3",
-    "info.cukes" % "cucumber-scala" % "1.1.3",
-    //Adding slick, and database stuff
-    "com.typesafe.slick" %% "slick" % "1.0.1-RC1",
-    "com.h2database" % "h2" % "1.3.166",
-    "postgresql" % "postgresql" % "9.1-901.jdbc4"
-    )
+libraryDependencies ++= Dependencies.jugbot
 
 resolvers ++= Seq( "snapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
     "spray repo" at "http://repo.spray.io",
---
+++ project/dependencies.scala
@@ -0,0 +1,65 @@
+import sbt._
+import Keys._
+
+object Library {
+  //versions
+  val sprayVersion = "1.1-M8"
+  val akkaVersion = "2.1.4"
+  val cucumberVersion = "1.1.3"
+  val logbackVersion = "1.0.13"
+  val sprayJsonVersion = "1.2.5"
+  val dispatchVersion = "0.10.1"
+  val scalaTestVersion = "1.9.1"
+  val flywayVersion = "2.1.1"
+  val typesafeConfigVersion = "1.0.2"
+  val postgresqlVersion = "9.1-901.jdbc4"
+  val slickVersion = "1.0.1-RC1"
+  val h2Version = "1.3.166"
+
+  //testing dependencies
+  val dispatch = "net.databinder.dispatch" %% "dispatch-core" % dispatchVersion
+  val scalaTest = "org.scalatest" % "scalatest_2.10" % scalaTestVersion
+  val sprayTestkit = "io.spray" % "spray-testkit" % sprayVersion
+  val cucumberJunit = "info.cukes" % "cucumber-junit" % cucumberVersion
+  val cucumberScala = "info.cukes" % "cucumber-scala" % cucumberVersion
+
+  //Primary app depends
+  val akkaActor = "com.typesafe.akka" %% "akka-actor" % akkaVersion
+  val akkaSlf4j = "com.typesafe.akka" %% "akka-slf4j" % akkaVersion
+  val logbackClassic = "ch.qos.logback" % "logback-classic" % logbackVersion
+  val sprayCan = "io.spray" % "spray-can" % sprayVersion
+  val sprayRouting = "io.spray" % "spray-routing" % sprayVersion
+  val sprayJson = "io.spray" %% "spray-json" % sprayJsonVersion
+  val flyway = "com.googlecode.flyway" % "flyway-core" % flywayVersion
+  val typesafeConfig = "com.typesafe" % "config" % typesafeConfigVersion
+
+  val postgresql = "postgresql" % "postgresql" % postgresqlVersion
+  val h2 = "com.h2database" % "h2" % h2Version
+  val slick = "com.typesafe.slick" %% "slick" % slickVersion
+
+
+}
+
+object Dependencies {
+
+  import Library._
+
+  val jugbot = List(
+    dispatch % "test",
+    scalaTest % "test",
+    sprayTestkit % "test",
+    cucumberJunit % "test",
+    cucumberScala % "test",
+    akkaActor,
+    akkaSlf4j,
+    logbackClassic,
+    sprayCan,
+    sprayRouting,
+    sprayJson,
+    flyway,
+    typesafeConfig,
+    h2,
+    postgresql,
+    slick
+  )
+}
